9b8941a5ef6f3158a57c33b8e2d31df13a481496
edman manigat <e.manigat@gmail.com>
2018-11-08 22:24:38 -0400

    Update FirstProject/App.js

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from "react";
-import { AppRegistry, View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, View } from "react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello EdMan This is your App!</Text>
-      </View>
-    );
-  }
-}
+import { TableNavigatior } from "react-navigation";
+import FirstScreen from './tabs/FirstScreen'
+import SecondScreen from './tabs/SecondScreen'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    backgroundColor: "#003c81",
-    color: "#4A90E2",
-    fontSize: 24,
-    padding: 10
-  }
+var MainScreenNavigator = TableNavigatior({
+  Tab1:{FirstScreen},
+  Tab2:{SecondScreen}
 });
 
-AppRegistry.registerComponent("App", () => App);
+MainScreenNavigator.navigatorOptions = {
+  title: "Tab example"
+};
+
+export default MainScreenNavigator;
